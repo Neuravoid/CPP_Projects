273fb21243518861ad13ae1dccff857d40cae05a
Hope <alkanumut848@gmail.com>
2026-08-22 14:33:47 +0300

    CPP09/ex00

---
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,156 @@
+#include "BitcoinExchange.hpp"
+
+static bool checkDate(long year, long month, long day){
+    if ((year > 2030 || year < 1990) ||
+    (month > 12 || month < 1) ||
+    (day < 1 || day > 31)
+    )
+    {
+        return 0;
+    }
+
+    if (
+        ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31) ||
+        (year % 4 == 0 && month == 2 && day > 29) ||
+        (year % 4 != 0 && month == 2 && day > 28)
+    )
+    {
+        return 0;
+    }
+    return 1;
+}
+
+static int seperateAndSave(std::map<std::string,float>& mapExample,std::string line, char type)
+{
+    int index = line.find(type);
+    std::string leftPart;
+    std::string rightPart;
+    leftPart = line.substr(0,index);
+    rightPart = line.substr(index+1);
+    double fRight = atof(rightPart.c_str());
+    if ((type == ',' && (fRight > 2147483647 || fRight < 0)) ||
+        (type == '|' && (fRight > 1000 || fRight < 0))
+    )
+    {
+        std::cout << fRight << " Number Too High or Too Low" << std::endl;
+        return 1;
+    }
+    long year = atol(leftPart.substr(0,leftPart.find("-")).c_str());
+    long month = atol(leftPart.substr(leftPart.find("-")+1,2).c_str());
+    long day = atol(leftPart.substr(leftPart.find("-")+4,2).c_str());
+    if (!checkDate(year,month,day))
+    {
+        std::cout << "Wrong Number" << std::endl;
+        return 1;
+    }
+    mapExample[leftPart] = static_cast<float>(fRight);
+    return 0;
+}
+
+void BitcoinExchange::getData(std::string fileName)
+{
+    std::fstream file(fileName.c_str());
+    if (!file)
+    {
+        std::cout << "Data File Couldn't Opened." << std::endl;
+    }
+    std::string line;
+    std::getline(file,line);
+    while (std::getline(file,line))
+    {
+        seperateAndSave(this->data,line,',');
+    }
+}
+
+
+
+std::map<std::string,float>::iterator BitcoinExchange::returnBound(std::string date){
+    return(this->data.lower_bound(date));
+}
+
+void BitcoinExchange::getInput(std::string fileName)
+{
+
+    std::fstream file(fileName.c_str());
+    if (!file)
+    {
+        std::cout << "Input File Couldn't Opened." << std::endl; 
+    }
+    float value;
+    int result;
+    std::map<std::string,float>::iterator data_it = this->startPtr('d');
+    std::map<std::string,float>::iterator data_ite = this->endPtr('d');
+
+    std::string line;
+    std::getline(file,line);
+    if (line.compare("date | value") != 0)
+    {
+        std::cout << "File Has Wrong Header." << std::endl;
+    }
+    while (std::getline(file,line))
+    {
+        result = seperateAndSave(this->input,line,'|');
+        std::map<std::string,float>::iterator input_ite = this->endPtr('i');
+        if (result == 0)
+        {
+            while (data_it != data_ite)
+            {           
+                if ((input_ite->first) == data_it->first)
+                {                
+                    value = input_ite->second * data_it->second;
+                    std::cout << std::fixed << std::setprecision(2) << input_ite->first << " => " << value << std::endl;
+                }
+                data_it++;
+                }
+                if (input_ite->first != data_it->first)
+                {
+
+                    std::map<std::string,float>::iterator behindIt =  this->returnBound(input_ite->first);
+                    value = behindIt->second * input_ite->second;
+                    std::cout << std::fixed << std::setprecision(2) << this->input.begin()->first << " => " << value << std::endl;
+                }
+        }
+    }
+}
+
+BitcoinExchange::BitcoinExchange(){
+    getData("data.csv");
+    getInput("input.txt");
+    std::cout << "Constructor Called" << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange(std::string fileName, std::string inputName){
+    getData(fileName);
+    getInput(inputName);
+    std::cout << "Constructor Called" << std::endl;
+}
+
+BitcoinExchange::~BitcoinExchange(){
+    std::cout << "Destructor Called" << std::endl;
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& ref){
+    this->data = ref.data;
+    return *this;
+    std::cout << "Copy Assignmant Operator Called" << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& ref){
+    this->data = ref.data;
+    std::cout << "Copy Constructor Called" << std::endl;
+}
+
+
+std::map<std::string,float>::iterator BitcoinExchange::startPtr(char type){
+    if (type == 'i')
+        return(this->input.begin());
+    return(this->data.begin());
+    
+}
+
+
+std::map<std::string,float>::iterator BitcoinExchange::endPtr(char type){
+    if (type == 'i')
+        return(this->input.end());
+    return(this->data.end());
+}
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -2,5 +2,29 @@
 #define BITCOINEXCHANGE_HPP
 
 
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <map>
+#include <exception>
+#include <stdlib.h>
+#include <iomanip>
+
+class BitcoinExchange{
+    private:
+        std::map<std::string,float> input;
+        std::map<std::string,float> data;
+        void getData(std::string fileName);
+        void getInput(std::string fileName);
+    public:
+        BitcoinExchange();
+        ~BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange& ref);
+        BitcoinExchange(std::string fileName,std::string inputName);
+        BitcoinExchange& operator=(const BitcoinExchange& ref);
+        std::map<std::string,float>::iterator startPtr(char type);
+        std::map<std::string,float>::iterator endPtr(char type);  
+        std::map<std::string,float>::iterator returnBound(std::string date);
+};
 
 #endif
